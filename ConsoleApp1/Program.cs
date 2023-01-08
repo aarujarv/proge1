78278a8a22d4733f33aee493fce52eb96c1c3f9d
aarujarv <a.arujarv@gmail.com>
2023-01-08 18:53:35 +0200

    new

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("How many steps have you walked today?");
+int usersSteps = Int32.Parse(Console.ReadLine());
+
+string result = HealthMonitor(usersSteps);
+
+Console.WriteLine(usersSteps);
+
+static string  HealthMonitor(int steps)
+{
+    if (steps < 4000)
+    {
+
+        return "You need to move more.";
+    }
+    else if (steps >= 4000 && steps < 8000)
+    {
+
+        return "Health goal achieved,";
+
+    }
+    else
+    {
+
+        return "Well done!";
+
+    }
+}
